e8c2a34fa64e8e4ee213bc6b85db915f3fa9d172
robsonpereiradasilva <robson.silva@novaandradina.org>
2022-11-14 21:01:58 -0400

    Add SalesCard component

--- frontent/src/App.tsx
+++ frontent/src/App.tsx
@@ -1,14 +1,20 @@
 import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
-
-
+import SalesCard from "./components/SalesCard"
 
 function App() {
    return (
-      <>      
-         <h1>Projeto Spring React</h1>
-         <Header/> 
-         <NotificationButton />
+      <>             
+      <Header/> 
+      <main>
+         <section id="sales">
+            <div className="dsmeta-container">
+               <SalesCard/>
+            </div>
+
+         </section>
+      </main>
+        
       </>
    )
 }
