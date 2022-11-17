3689f5fb9b03a59d2b7671d78d82b2959ade17eb
robsonpereiradasilva <robson.silva@novaandradina.org>
2022-11-17 19:01:14 -0400

    Add useCase Sales

---
+++ backend/src/main/java/com/rpstech/venda/controller/SaleController.java
@@ -0,0 +1,41 @@
+package com.rpstech.venda.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rpstech.venda.entities.Sale;
+import com.rpstech.venda.services.SaleService;
+import com.rpstech.venda.services.SmsService;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+	
+	@Autowired
+	private SaleService service;
+	
+	@Autowired
+	private SmsService smsService;
+	
+	@GetMapping
+	public Page<Sale> findeSales(
+		@RequestParam(value="minDate", defaultValue="")	String minDate,
+		@RequestParam(value="minDate", defaultValue="")	String maxDate, 
+		Pageable pageable){
+		return service.findSales(minDate, maxDate, pageable);
+	}
+	
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id ) {
+		smsService.sendSms(id);
+	}
+	 
+	
+
+}
